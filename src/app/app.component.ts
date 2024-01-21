0ab379ec6f280898798cd9b8af1ccfd1f4d234aa
Eduardo347387 <143133753+Eduardo347387@users.noreply.github.com>
2024-01-21 15:06:00 -0600

    add archive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./_appComponent.scss']
 })
 export class AppComponent {
   title = 'pageCountries';
